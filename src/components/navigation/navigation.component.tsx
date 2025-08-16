cf5a9cb9039abde7c7c9ec7617749026094928d5
calitovilla <calitovilla@users.noreply.github.com>
2025-08-16 17:24:00 +0200

    fragment

--- src/components/navigation/navigation.component.tsx
+++ src/components/navigation/navigation.component.tsx
@@ -1,16 +1,19 @@
 import { Outlet } from 'react-router'
+import { Fragment } from 'react';
 
 function Navigation() {
   return (
-    <nav>
-      <h1>Plant E-commerce</h1>
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/plants">Plants</a></li>
-        {/* Add more navigation links as needed */}
-      </ul>
-      <Outlet />
-    </nav>
+    <Fragment>
+        <nav>
+        <h1>Plant E-commerce</h1>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/plants">Plants</a></li>
+            {/* Add more navigation links as needed */}
+        </ul>
+        </nav>
+        <Outlet />
+    </Fragment>
   )
 }
 
