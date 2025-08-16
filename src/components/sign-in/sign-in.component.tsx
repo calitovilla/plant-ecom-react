88ef38d852c25ff114f600fc6cea4e9eea5871f0
calitovilla <calitovilla@users.noreply.github.com>
2025-08-16 20:00:28 +0200

    authentication with google

--- src/components/sign-in/sign-in.component.tsx
+++ src/components/sign-in/sign-in.component.tsx
@@ -1,7 +1,25 @@
+import { signInWithGooglePopup } from '../../utils/firebase/firebase.utils';
+
+
 const SignIn = () => {
+
+    const handleSignIn = async () => {
+        try {
+            const response = await signInWithGooglePopup();
+            console.log(response);
+            // Handle successful sign-in here (e.g., redirect or show a message)
+        } catch (error) {
+            console.error("Error signing in with Google:", error);
+            // Handle sign-in error here (e.g., show an error message)
+        }
+    };
+
   return (
     <div>
       <h2>Sign In</h2>
+        <button onClick={handleSignIn}>
+            Sign in with Google
+        </button>
     </div>
   );
 };
